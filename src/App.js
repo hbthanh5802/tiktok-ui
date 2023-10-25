11fc16fe8d2f424899d8ff9cb8978db71dc4ef4d
ThanK <hbthanh582002@gmail.com>
2023-10-25 14:45:42 +0700

    Update Layout/Routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
                         let Layout = DefaultLayout;
                         if (route.layout) {
                             Layout = route.layout;
-                        } else if (route.layout == null) {
+                        } else if (route.layout === null) {
                             Layout = Fragment;
                         }
                         const Page = route.component;
